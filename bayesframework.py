891ed3fa19ca98664bec9b4072c1259a0803ee2e
Hannah Nelson <nexec64@gmail.com>
2024-07-03 04:44:31 -0700

    Seriously simplified the object design. Implemented algorithm for finding conditional probability. Currently untested.

--- bayesframework.py
+++ bayesframework.py
@@ -3,132 +3,109 @@ Bayes Framework
 
 This module is allows simple Bayesian modeling of conditionally connected catagorical distributions. It models a causal chain of random variables which are related by markov/emission matricies. This can be used for things which are not markov chains!
 
-To utilize this library follow the following procedure:
-    create a initial node,
+To utilize this library to find conditional probability follow the following procedure:
+    create an initial root node via the constructor
     create the connections to that node via Node.add_connection()
-    initialize the prior of the root node with Node.initialize_prior
-    record observations using Node.observe
-    retrieve desired distributions from desired node using Node.distribution
+    mark down observations for known nodes using Node.observe()
+    initialize the prior of the root node with Node.solve_probabilities(<prior of the root node>)
+    retrieve desired distributions from desired node using the Node.distribution attribute
     
 Notes: initialize_prior is only intended to be used on the root node, all other nodes will automatically initialize when needed based on the prior given to the root, or updates made.
+
 """
+#TODO implement viturbi's algorithm so we can also find the most likely hidden states.
 # 
 import numpy as np
 from dataclasses import dataclass, field
 @dataclass
 class Node(): #  
     #  attributes!
-    __distribution: np.array = field(init=False)
-    cause: 'Node' = field(default=None)
-    cause_joint: 'Joint' = field(default=None)
-    effect: list['Node'] = field(init=False, default_factory=list)
-    effect_joint: list['Joint'] = field(init=False, default_factory=list)
-    observed: bool = field(init=False, default=False)
+    effects: list['Effect'] = field(init=False, default_factory=list)
+    distribution: np.array = field(init=False, default=None)
+    observation: int = field(init=False, default=None)
+    number_of_catagories = field(kw_only=True, default=None)
     # 
-    def add_connection(self, *, relation: np.array, relation_type ='emission') -> 'Node': #  
+    def add_connection(self, emission_matrix: np.array) -> 'Node': #  
         """ #  
-        Creates a new branch off from the existing causal tree.
+        Creates a new branch off from the existing causal tree. Returns the created node.
 
-        Returns the created node.
-
-        relation: np.array
-            - if relation type is \'emission\' this is an emission matrix which specified the relation between cause node and effect node
-        relation_type
-            \'emission\' - 
-            \'joint\' - 
+        emission_matrix: np.array
+            - the emission matrix which relates the cause to the effect
         """
         # 
-        joint = None
-        if relation_type='emission': #  
-            joint = Joint(relation)
+        #  validate emission matrix
+        if emission_matrix.ndim != 2:
+            raise ValueError('Emission matricies must have a dimensionality of 2')
         # 
-        elif relation_type='joint': #  
-            joint = relation
+        #   deal with number of catagories not being set
+        if self.number_of_catagories == None:
+            self.number_of_catagories = emission_matrix.shape[1]
         # 
-        else: #  
-            raise ValueError('Arguement \'relation_type\' provided with invalid input {}'.format(relation))
+        #  deal with number of catagories not matching up
+        if self.number_of_catagories != emission_matrix.shape[1]:
+            raise ValueError('Emission matrix is of incompatible size. Emission matrix has input size of {} but node has {} catagories'.format(emission_matrix.shape[1], self.number of catagories))
         # 
-        new_node = Node(cause=self, cause_joint=Joint(emission_matrix))
-        effect.append(new_node)
-        pass #TODO
+        #  create the connection
+        new_node = Node(number_of_catagories=emission_matrix.shape[0])
+        effects.append(Effect(new_node, emission_matrix, None))
+        # 
+        #  return the new node
+        return new_node
+        # 
     # 
-    def initialize_prior(self, prior: np.array): #  
-        self.__distribution = prior
+    def solve_probabilities(self, prior: np.array) #  
+        """ #  
+        Finds the conditional probability of every state of every node given the observations
+        """
+        # 
+        #  propagate the probability in two swoops!
+        self.distribution = prior
+        self.__bayes_propagate_up()
+        self.__bayes_propagate_down()
+        # 
     # 
-    @property #  
-    def distribution(self):
-        if self.__distribution == None:
-            self.__distribution = cause[1]. @ cause[0]
-        return self.__distribution
+    def __bayes_propagate_up(self): #  
+        """
+        Propagates probability such that every causal node has the correct probability for all nodes which are its effects
+        """
+        for effect in effects:
+            effect.joint_matrix = effect.emission_matrix * self.distribution[None,:]
+            initial_effect_distribution = np.sum(effect.joint_matrix, axis=1)
+            effect.node.distribution = initial_effect_distribution
+            effect.node.__bayes_propagate_up()
+            effect.joint_matrix = effect.joint_matrix *\
+                    (effect.node.distribution/initial_effect_distribution)[:,None]
+            self.distribution = np.sum(effect.joint_distribution, axis=0)
     # 
-    def observe(self, distribution: np.array): #  
-        pass #TODO
+    def __bayes_propagate_down(self): #  
+        """
+        propagates probability such that every node's probability becomes consistent with the changes which may have occured above it due to other branches!
+        """
+        for effect in effects: 
+            joint_cause_distribution = np.sum(effect.joint_matrix, axis=0)
+            effect.joint_matrix = effect.joint_matrix *\
+                    (self.distribution/joint_cause_distribution)[None,:]
+            effect.node.distribution = np.sum(effect.joint_matrix, axis=1)
+            effect.node.__bayes_propagate_down()
     # 
-    @property #  
-    def dim(self):
-        return dim(distribution)
+    def observe(self, observation: int): #  
+        """
+        mark down an observed outcome for a particular 
+        """
+        self.observation = observation
+    # 
+    def clear_observations(self): #  
+        """
+        Clears all observations. This is usefull when you want to find the result for something with the same causal layout but different emission probabilities
+        """
+        self.observation = None
+        for node in effect_node:
+            effect_node.clear_observations
     # 
 # 
-class Joint(): #  
-    """ #  
-    This class represents a connection between two prior nodes. It represents the joint ditribution.
-
-    for purposes of organization I am declaring cause the top and effect the side. Eg: the cause prior is the sum accross the columns, the effect prior is sum across the rows.
-
-    emission matrix indicates an emmision matrix or a markov matrix, since all markov matricies are emisrion matricies
-    """
-    # 
-    #  attributes
-    cause_prior: np.array
-    effect_prior: np.array
+@dataclass #  
+class Effect():
+    node: 'Node'
     emission_matrix: np.array
-    joint_distribution: np.array
-    # 
-    def __init__(self, *, relation=None, relation_type='emission'): #  
-        #  verify relation type!
-        if relation_type='emission': #  
-            joint = Joint(relation)
-        # 
-        elif relation_type='joint': #  
-            joint = relation
-        # 
-        else: #  
-            raise ValueError('Arguement \'relation_type\' provided with invalid input {}'.format(relation))
-        # 
-        # 
-        """ #  
-        Constructor for Joint class
-
-        relation
-            - if relation_type is \'emission\' then relation is the emission matrix which relates the cause to the effect
-            - if relation type is \'joint\' then relation is the joint distribution between the cause and effect
-        """
-        # 
-        #  initialize values
-        self.cause_prior = None
-        self.effect_prior = None
-        self.emission_matrix = emission_matrix
-        self.joint_distribution = joint_distribution
-        # 
-        #  if we are given a joint distribution initialize the priors at this time!
-        if joint_distribution != None:
-            pass #TODO
-        # 
-    # 
-    def update_cause(self, new_cause_distribution: np.array): #  
-        #  if the new cause prior matches the old one don't bother updating
-        if np.all(new_cause_distribution==self.cause_prior):
-            return effect_prior
-        # 
-        #  initialize the joint distribution if needed
-        if self.joint_distribution == None:
-            pass #TODO
-        # 
-        #TODO
-    # 
-    def update_effect(self, new_effect_distribution: np.array): #  
-        if self.joint_distribution == None:
-            raise RuntimeError('Upwards Bayesian propagation requested before joint distribution could be established')
-        #TODO
-    # 
+    joint_matrix: np.array
 # 
